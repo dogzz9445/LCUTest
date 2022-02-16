88a22af7fb81f03d7e5a061c730d25828380fcfa
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2022-02-16 21:18:09 +0900

    edit layout

---
+++ LCUTest/ViewModel/RecordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace LCUTest.ViewModel
+{
+    public class RecordViewModel : BindableBase
+    {
+        private bool _isConncted;
+
+        public bool IsConncted { get => _isConncted; set => SetProperty(ref _isConncted, value); }
+    }
+}
